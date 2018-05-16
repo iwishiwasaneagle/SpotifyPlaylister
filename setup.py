21e9ddfa83b57f33afc4dabb74dc491423d6247e
iwishiwasaneagle <jh.ewers@gmail.com>
2018-05-17 00:10:27 +0100

    Some shiz

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SpotipyPlaylister',
+    version='01.dev',
+    author='JH Ewers',
+    author_email='jh.ewers@gmail.com',
+    url='https://github.com/iwishiwasaneagle/SpotifyPlaylister',
+    description='Keyboard shortcut script to easily add songs to a predefined playlist',
+    long_description=open('README.md').read(),
+    install_requires='requirements.txt'
+)
