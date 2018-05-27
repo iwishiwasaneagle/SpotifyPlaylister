690e56ce6014bccc2b4489136c20e1712dc6e40f
iwishiwasaneagle <jh.ewers@gmail.com>
2018-05-27 20:15:42 +0100

    Updated to python3

--- keypress.sh
+++ keypress.sh
@@ -4,4 +4,4 @@
 # Only exists because if you hook a keybinding to a python script, it will run it as root and fuck shit up.
 # This way we can ensure that the correct working directory is used
 cd /home/jan-hendrik/Documents/Code/Spotipi
-python SpotiPi_main.py
+python3 SpotiPi_main.py
